c69c0fc554a35136af338a0876837abcdd63ecd8
Haeun <haeun930515@gmail.com>
2022-11-18 17:06:36 +0900

    Photo View Model 추가 Bottom Navigation 변경 navigation graph 수정

--- app/src/main/java/com/example/kakaoimageai/presentation/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/kakaoimageai/presentation/view/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kakaoimageai.presentation.view.activity
 
+import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -14,13 +15,12 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main){
 
     private val mainViewModel : MainViewModel by viewModels()
 
-
     override fun initView() {
         super.initView()
-
-//        binding.apply {
-//            val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_frame_layout) as NavHostFragment
-//            bottomNavigationView.setupWithNavController(navHostFragment.navController)
-//        }
+        binding.apply {
+            val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_frame_layout) as NavHostFragment
+            bottomNavigationView.setupWithNavController(navHostFragment.navController)
+        }
     }
+
 }
---
+++ app/src/main/java/com/example/kakaoimageai/presentation/view/base/BaseFragment.kt
@@ -0,0 +1,25 @@
+package com.example.kakaoimageai.presentation.view.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment <VB: ViewDataBinding>(@LayoutRes private val resId: Int) : Fragment(){
+
+    lateinit var binding: VB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater,resId,container,false)
+        return binding.root
+    }
+
+}
