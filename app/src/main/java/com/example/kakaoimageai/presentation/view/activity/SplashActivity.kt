ad5c49cd2ad6999ac749fa9cc3b03923c9ecdda1
kyungbbabi <kyung981124@naver.com>
2022-11-25 16:19:25 +0900

    스플래쉬액티비티 추가

---
+++ app/src/main/java/com/example/kakaoimageai/presentation/view/activity/SplashActivity.kt
@@ -0,0 +1,9 @@
+package com.example.kakaoimageai.presentation.view.activity
+
+import com.example.kakaoimageai.R
+import com.example.kakaoimageai.databinding.ActivitySplashBinding
+import com.example.kakaoimageai.presentation.view.base.BaseActivity
+
+class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash){
+
+}
